1889a2ed7f49d6180c9dc31733e16f8ebc644cdf
alirex <alirex.prime@gmail.com>
2022-09-09 20:44:56 +0300

    ✨ Add feature

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ def print_hi(name):
     print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def printHello(name):
+def print_hello(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
+    print_hi("PyCharm :)")
