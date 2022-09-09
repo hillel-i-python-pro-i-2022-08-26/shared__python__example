6b91611f0eb879646b973f6ec4c069eeb1c6055a
alirex <alirex.prime@gmail.com>
2022-09-09 20:28:38 +0300

    ✨ 3nd

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-def print_hi(
-
-
-        name
-
-
-
-):
+def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {  name}")  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def printHello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-
-
-
-
-
     print_hi("PyCharm")
